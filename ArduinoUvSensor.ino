ac1ce81438b2f25d67e4b66d4ab859db03a95572
Rasmus isager kruuse <isager-kruuse@hotmail.com>
2018-04-16 13:34:56 +0200

    switched to using multiplexer, we can now decide which analog input to read from, and whether to use a pull up resistor

--- ArduinoUvSensor.ino
+++ ArduinoUvSensor.ino
@@ -1,3 +1,8 @@
+// tasks left 
+// make accelerometer woprk
+// medianfiltering on the distance sensor
+
+
 #include <MedianFilter.h>
 #include <SharpDistSensor.h>
 #include <SPI.h> //communicate with the shield
@@ -6,8 +11,16 @@
 #define SSDSHIELD // If this is defined the code will assume the SSD SHIELD is attached if not it will not compile any logging into the code 
 #define DEBUG // defining this makes the program print to the serial monitor, not defining it prevents the any printing functions from being complied into the code
 
-//class for holding the user data
+#define multiplexPin A0 // pin that the multiplexer is connected to 
+int bitPin[4] = {4,5,6,7}; // pins used to which multiplexer channel to read from 
+int pinTable[16][4]; // table to hold the states of bits need to acess a particulair channel
 
+//chanels for the different sensors
+int accCh[3] ={0,1,2}; 
+int irCh[3] ={3,4,5};
+int preCh[3] ={6,7,8};
+
+//class for holding the user data
 class User{
   public:
   unsigned long flushStamp; // time stamp for when the flush is pressed
@@ -23,35 +36,15 @@ class User{
 };
 
 
+
 //sensor variables
-#define IR_NUM     3 // Number or sensors.
-#define MAX_DISTANCE 300 // Max distance in cm.
-#define IRBUFFER 120 // what nr to read on the ir sensor to count as person, higher values makes the distance short exponentially
-
-// baseline variables, (keep these high because the sensor is utter shite)
-#define DYNAMICBASELINE false // wheather or not to adjust baseline on the fly(not implemented yet).
-#define SAMPLESFORBASELINE 50
-
-#define BUFFER 5 //How far away from baseline data does a result have to be to not snap to the baseline
-
-
+#define IRBUFFER 120 // what reading mean that hands are in front of the sensor
+#define ACCBUFFER 120 // what reading on the acc to count as flush
 
 //flow varaibles
 #define TIMEUNTILSTART 75; //how many milliseconds to wait before starting to meassure for the baseline
 
- 
-//sensor and pin stuff
-#define IR_PIN0  A0
-#define IR_PIN1  A1
-#define IR_PIN2  A2 
 
-#define ACC_PIN0  0
-#define ACC_PIN1  1
-#define ACC_PIN2  2
-
-#define PRE_PIN0 A3
-#define PRE_PIN1 A4
-#define PRE_PIN2 A5
  
 
 //user
@@ -68,8 +61,6 @@ bool baselineSet[IR_NUM];
 uint8_t currentSensor = 0; // Which sensor is active.
 
 //person detection variables
-unsigned int peopleCounter[IR_NUM] = {0,0,0}; //counts nr hold people entering
-unsigned int baselineCounter[IR_NUM] = {0,0,0};
 unsigned long timeIn[IR_NUM];
 
 //declare sensors
@@ -85,7 +76,7 @@ File myFile;
 
 void setup() {
   Serial.begin(9600);
-  
+  initMultiplexer(); // prep the multiplexer stuff
   // set the model of the ir sensors
   for(int i = 0; i < IR_NUM ; i++){
       irSensor[i].setModel(SharpDistSensor::GP2Y0A710K0F_5V_DS);
@@ -93,7 +84,6 @@ void setup() {
       baselineSamples[i] = 0;
       pinMode(prePin[i], INPUT);
   }
-  delay(2000); //delay for 5 secs before starting to set the baseline
   #ifdef SSDSHIELD
   //Initialize SD card and Check if the SD card and libary failed
   Serial.print("Initializing SD card...");
@@ -126,34 +116,13 @@ void setup() {
   #endif
 }
 
-//Requires pin A0 to be a pullup, returns soap value
-
-int soapPressure()
-{
-  pinMode(A0, INPUT_PULLUP);
-  
-  int sensorValue = analogRead(A0);
-
-  if(sensorValue < 100)
-  return 1;
-  else
-  return 0;
-  
-}
-
 void loop() {
-  // check for flush
-  // if flushed save user data then clear it
-
-  // ir distance reading
-  for (uint8_t i = 0; i < 1; i++) {
-      
-      currentSensor = i;
-
+// for each wired bathroom
+  for (uint8_t i = 0; i < 3; i++) {
+      // i is used to refer to the sensors attached to the current bathroom
       // start by checking if there is a flush, and if its been more than 12 seconds since the last flush
-      // marc's code goes
-      
-      if (accReading >=  1 &&  millis() > user[i].flushStamp + 12000 ){
+
+      if (isFlush(i) &&  millis() > user[i].flushStamp + 12000 ){
         //if it has it must be a new user in, open the correct file
         #ifdef SSDSHIELD
         myFile = SD.open("log" + String(i) + ".txt", FILE_WRITE);
@@ -166,29 +135,86 @@ void loop() {
         user[i].flushStamp = millis(); // and that user flushed just now
       }
       // check the proximity sensor on the tabs
-      reading[currentSensor] = irSensor[currentSensor].getDist();
-      if(reading[i] > IRBUFFER){
+      if(irDist(i)){
         if(user[i].initHW == 0){
          user[i].initHW = millis();
         }
-      user[i].lastHW = millis();
+        user[i].lastHW = millis();
       }
-      // Check the soap
-      // kevin code goes 
-      int presReading = analogRead(prePin[i]);
-      if(presReading != 0 && user[i].soapStamp == 0){
+      // Check the soap 
+      if(soapPressure(i) && user[i].soapStamp == 0){
         user[i].soapStamp = millis();
       }
       
     #ifdef DEBUG //prints only the information if debug mode is declared
     //Serial.println("for user " + String(i)+ " :  " + String(user[i].flushStamp)+ "  " + String(user[i].initHW) + "  " + String(user[i].lastHW) + "  " + String(user[i].soapStamp));
     Serial.println(digitalRead(0));
-    if(digitalRead(0) == 1){
-      delay(2000);
-    }
     #endif
   }  
-  // The rest of your code would go here.
+}
 
+int multiplexRead(int pin, bool usingPullUp){
+  //setting the bit pins
+  for(int i = 0; i < 4; i ++){
+    if(pinTable[pin][i] != 0){
+    digitalWrite(bitPin[i], HIGH);
+    }else{
+    digitalWrite(bitPin[i], LOW);
+    }
+  }
+  // check if using pull up
+  if(usingPullUp){
+    pinMode(multiplexPin, INPUT_PULLUP);
+  }else{
+    pinMode(multiplexPin, INPUT);
+  }
+  delay(1); // give the arduino a msecond to account for the changeins in pull up
+  return analogRead(multiplexPin); // read the pin an return the results
+}
+
+void initMultiplex(){
+  for(int i = 0; i < 4; i++){
+    pinMode(bitPin[i], OUTPUT);
+  }
+  for(int i = 0; i < 9; i++){
+    int nr = i;
+    if(nr - 8 >= 0){
+      pinTable[i][3] = 1;
+      nr -= 8;
+    }
+    if(nr - 4 >= 0){
+      pinTable[i][2] = 1;
+      nr -= 4;
+    }
+    if(nr - 2 >= 0){
+      pinTable[i][1] = 1;
+      nr -= 2;
+    }
+    if(nr - 1 >= 0){
+      pinTable[i][0] = 1;
+      nr -= 1;
+    }
+    //Serial.println("for " + String(i) + " : " + String(pinTable[i][3]) + " " + String(pinTable[i][2]) + " " + String(pinTable[i][1]) + " " + String(pinTable[i][0]));
+  }
+}
+
+
+bool soapPressure(int nr)
+{ 
+  int sensorValue = multiplexRead(preCh[nr], true);
+  if(sensorValue < 100){return true;}
+  return false;
+}
+
+bool irDist(int nr){
+  int sensorValue = multiplexRead(irCh[nr], false);
+  if(sensorValue > IRBUFFER){return true;}
+  return false;
+}
+
+bool isFlush(int nr){
+  int sensorValue = multiplexRead(accCh[nr], false);
+  if(sensorValue > IRBUFFER){return true;}
+  return false;
 }
 
