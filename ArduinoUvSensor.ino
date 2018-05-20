71e0b13d2891a58976238bf8f6feaaa8a7e489fe
Rasmus isager kruuse <isager-kruuse@hotmail.com>
2018-05-20 19:00:26 +0200

    SWitched to new flow for logging toilet use

--- ArduinoUvSensor.ino
+++ ArduinoUvSensor.ino
@@ -3,53 +3,151 @@
 #include <SD.h> // SD read and write functions
 // IMPORTANT STATE READ WHAT THEY DO
 #define SSDSHIELD // If this is defined the code will assume the SSD SHIELD is attached if not it will not compile any logging into the code 
-//#define DEBUG // defining this makes the program print to the serial monitor, not defining it prevents the any printing functions from being complied into the code
+#define DEBUG // defining this makes the program print to the serial monitor, not defining it prevents the any printing functions from being complied into the code
 
-//sensor variables
-#define userNr 3
-#define windowSize 9 // size to use for median filtering of the ir sensors 
-#define windowSizeAcc 3 // filter size to use for the accelerometer 
-
-#define IRBUFFER 120 // what reading mean that hands are in front of the sensor
-#define ACCBUFFER 360 // what reading on the acc to count as flush
-#define FLUSHBUFFER 120
-
-//filters for the noisy transducers Inferred and Accelerometer
-MedianFilter irFilter[3] = {MedianFilter(windowSize,0), MedianFilter(windowSize,0), MedianFilter(windowSize,0)};
-//MedianFilter accFilter[3] = {MedianFilter(windowSizeAcc,0), MedianFilter(windowSizeAcc,0), MedianFilter(windowSizeAcc,0)};
 
 //chanels for the different sensors
-int irCh[3] ={A0,A1,A2};
-int preCh[3] ={A3,A4,A5};
-int accCh[3] ={A6,A7,A8};
+int irPins[3] = {A0,A1,A2};
+int soapPins[3] = {A3,A4,A5};
+int flushPins[3] = {A6,A7,A8};
 
-//accelerometer controls
-int prevAccReading[3] = {300,300,300}; // holds the last accelerometer reading for comparison
-int accBuf[3] = {60,60,60}; // how much difference between readings to count as lever movement, controls how sensitive the accelerometer is try to stay above 3 if possible
-
+//timer for debuging via serial
+unsigned long timer = 0;
 
 //class for holding the user data
 class User{
   public:
   unsigned long flushStamp; // time stamp for when the flush is pressed
-  unsigned long initHW; //initial handwashing time
-  unsigned long lastHW; //last time the handwashing sensor is pressed
+  bool hasFlushed;
+  unsigned long initHw; //initial handwashing time
+  bool hasHw;
+  unsigned long lastHw; //last time the handwashing sensor is pressed
+  bool hasHwSoap;
   unsigned long soapStamp; //time stamp when soap is pressed
-  User::User(){
-  flushStamp = 0;
-  initHW = 0;
-  lastHW = 0;
-  soapStamp = 0;
+  bool hasSoap;
+  unsigned long lastActive;
+  bool currentlyActive ; // 
+  bool activity ; // check if any of the sensor show activity
+
+  // for SD stuff
+
+  File userFile;
+  int userNumber = 0; 
+
+  // thresholds
+
+  const int irThreshold = 220;
+  int soapThreshold = 50;
+  const int flushThreshold = 650;
+
+  //reset time 
+
+  const int resetTime = 20000;
+
+  // median filter for ir sensor
+  MedianFilter filter = MedianFilter(9,0);
+  
+  User(int myNumber, int soapTHRESH){
+    soapThreshold = soapTHRESH;
+    userNumber = myNumber;
+    flushStamp = 0;
+    hasFlushed = false;
+    initHw = 0;
+    hasHw = false;
+    lastHw = 0;
+    soapStamp = 0;
+    hasSoap = false;
+    hasHwSoap = false;
+    lastActive = 0;
+    activity = false;
+
   };
+
+  void readFromSensors(int flushPin, int handPin, int soapPin){
+    activity = false;
+    
+    if(millis() > (lastActive + resetTime) && lastActive != 0){
+      //reset user
+      #ifdef SSDSHIELD
+        userFile = SD.open("log" + String(userNumber) + ".txt", FILE_WRITE);
+        if(userFile){// if opened sucessfully save data 
+          userFile.println(String(flushStamp) + " " + String(initHw) + " " + String(lastHw) + " " + String(soapStamp));
+        }
+        userFile.close();
+      #endif
+      resetClass();
+    }
+
+    if((analogRead(flushPin) > flushThreshold) && !hasFlushed){
+      hasFlushed = true;
+      flushStamp = millis();
+      activity = true;
+    }
+
+    if((analogRead(soapPin) < soapThreshold) && !hasSoap){
+      hasSoap = true;
+      soapStamp = millis();
+      activity = true;
+    }
+
+    int distVal = analogRead(handPin);
+    filter.in(distVal);
+    distVal = filter.out();
+    
+    if(distVal > irThreshold){
+      activity = true;
+      if(!hasSoap && !hasHw ){
+        hasHw = true;
+        initHw = millis();
+      }
+      // if they used soap they are done
+      if(hasSoap){
+        //done complete
+        hasHwSoap = true;
+      }
+
+      lastHw = millis();
+
+    }else if(lastActive + 5000 < millis() && hasHwSoap){
+
+      //save the users data 
+
+      #ifdef SSDSHIELD
+        userFile = SD.open("log" + String(userNumber) + ".txt", FILE_WRITE);
+        if(userFile){// if opened sucessfully save data 
+          userFile.println(String(flushStamp) + " " + String(initHw) + " " + String(lastHw) + " " + String(soapStamp));
+        }
+        userFile.close();
+      #endif
+      Serial.println("im resetting ");
+      //reset user
+      resetClass();
+    }
+
+    if(activity){
+      lastActive = millis();
+    }
+    
+  }
+
+  void resetClass(){
+    flushStamp = 0;
+    hasFlushed = false;
+    initHw = 0;
+    hasHw = false;
+    lastHw = 0;
+    soapStamp = 0;
+    hasSoap = false;
+    hasHwSoap = false;
+    lastActive = 0;
+    activity = false;
+    // also save shit here
+  }
 };
 
 
 //user
-User user[3] = { User(),User(),User()};
-//person detection variables
-unsigned long timeIn[userNr];
-unsigned long timer = 0;
-int sensorValue;
+User user[3] = { User(0,50),User(1,300),User(2,200)};
 
 
 //class for doing SD shield related things 
@@ -92,44 +190,14 @@ void setup() {
 
 void loop(){
 // for each wired bathroom
-/*
-for(int i = 0 ; i < 3 ; i++){
-  Serial.print( analogRead(accCh[i]));
-  Serial.print( "    ") ;
-}
-Serial.println();*/
+  Serial.println(String(analogRead(A0)) + " " + String(analogRead(A1)) + " "  + String(analogRead(A2)) + "     " +String(analogRead(A3)) + " " + String(analogRead(A4)) + " " + String(analogRead(A5)) + "       " +String(analogRead(A6)) + " " + String(analogRead(A7)) + " "  + String(analogRead(A8)));
   for (uint8_t i = 0; i < 3; i++) {
-      // i is used to refer to the sensors attached to the current bathroom
-      // start by checking if there is a flush, and if its been more than 12 seconds since the last flush
-      //  
-      if (isFlush(i) && millis() > user[i].flushStamp + 12000){
-        //if it has it must be a new user in, open the correct file
-        #ifdef SSDSHIELD
-        myFile = SD.open("log" + String(i) + ".txt", FILE_WRITE);
-        if(myFile){// if opened sucessfully save data 
-          myFile.println(String(user[i].flushStamp) + " " + String(user[i].initHW) + " " + String(user[i].lastHW) + " " + String(user[i].soapStamp));
-        }
-        myFile.close();
-        #endif
-        user[i] = User();
-        user[i].flushStamp = millis(); // and that user flushed just now
-      }
-      // check the proximity sensor on the tabs
-      if(irDist(i)){
-        if(user[i].initHW == 0){
-         user[i].initHW = millis();
-        }
-        user[i].lastHW = millis();
-      }
-      // Check the soap 
-      if(soapPressure(i) && user[i].soapStamp == 0){
-        user[i].soapStamp = millis();
-      }
+    user[i].readFromSensors(flushPins[i],irPins[i],soapPins[i]);
   }  
   #ifdef DEBUG //prints only the information if debug mode is declared
   if(millis() > timer){
     for(int i = 0; i < 3; i++){
-    Serial.print("for user " + String(i)+ " :  " + String(user[i].flushStamp)+ "  " + String(user[i].initHW) + "  " + String(user[i].lastHW) + "  " + String(user[i].soapStamp) + "             ");
+    Serial.print("for user " + String(i)+ " :  " + String(user[i].flushStamp)+ "  " + String(user[i].initHw) + "  " + String(user[i].lastHw) + "  " + String(user[i].soapStamp) + " "  + String(user[i].lastActive) +"             ");
     }
     Serial.println();
     timer = millis() + 2000;
@@ -140,39 +208,11 @@ Serial.println();*/
 void initInput()
 {
   for(int i = 0; i < 3; i++){
-    pinMode(preCh[i], INPUT_PULLUP);
-    pinMode(irCh[i], INPUT);
-    pinMode(accCh[i], INPUT_PULLUP);
+    pinMode(soapPins[i], INPUT_PULLUP);
+    pinMode(irPins[i], INPUT);
+    pinMode(flushPins[i], INPUT_PULLUP);
   }
   
-}
-
-bool soapPressure(int nr)
-{ 
-  sensorValue = analogRead(preCh[nr]);
-  if(sensorValue < 250){return true;}
-  return false;
-}
-
-bool irDist(int nr)
-{
-  sensorValue = analogRead(irCh[nr]);
-  irFilter[nr].in(sensorValue);
-  sensorValue = irFilter[nr].out();
-  if(sensorValue > IRBUFFER){return true;}
-  return false;
-}
-
-bool isFlush(int nr)
-{
-  sensorValue = analogRead(accCh[nr]);
-  if(nr == 2){
-    if(sensorValue > 750){return true;}
-  return false;
-  }else{
-    if(sensorValue < FLUSHBUFFER){return true;}
-  return false;
-  }
 }
 
 
